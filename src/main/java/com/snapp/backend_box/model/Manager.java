98400fc73c211e6cf40afa564a5c0ca618864f99
a.parvaneh <amirparvaneh138@gmail.com>
2025-03-25 22:31:24 +0330

    [bug] fix the problem with data model design

---
+++ src/main/java/com/snapp/backend_box/model/Manager.java
@@ -0,0 +1,23 @@
+package com.snapp.backend_box.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "manager")
+@EqualsAndHashCode(callSuper = false)
+public class Manager extends BaseEntity{
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false,unique = true)
+    private String userName;
+    @Column(nullable = false)
+    private String password;
+}
