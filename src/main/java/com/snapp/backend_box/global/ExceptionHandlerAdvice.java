8a2b69f9834927ff456bf09d4097338275169cc9
a.parvaneh <amirparvaneh138@gmail.com>
2025-03-26 01:25:36 +0330

    [feature] implement services

--- src/main/java/com/snapp/backend_box/global/ExceptionHandlerAdvice.java
+++ src/main/java/com/snapp/backend_box/global/ExceptionHandlerAdvice.java
@@ -6,10 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
 import java.time.LocalDateTime;
 import java.util.Locale;
