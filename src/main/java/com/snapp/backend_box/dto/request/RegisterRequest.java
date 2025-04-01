b3ad4801a6d6765517fc913d0fdede8b39403576
a.parvaneh <amirparvaneh138@gmail.com>
2025-04-01 13:29:18 +0330

    [feature] solve bug of swagger

--- src/main/java/com/snapp/backend_box/dto/request/RegisterRequest.java
+++ src/main/java/com/snapp/backend_box/dto/request/RegisterRequest.java
@@ -5,15 +5,47 @@ import java.io.Serializable;
 public class RegisterRequest implements Serializable {
     private String username;
     private String password;
-    private String name;
-    private String type; // "manager", "customer", "biker"
+    private String firstName;
+    private String lastName;
+    private String type;
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
